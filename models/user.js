7d02facd2f1ffa63bc4122a5094698522fa9081d
codesterAlex <laalit.sunaar@gmail.com>
2018-11-21 19:03:48 +0545

    Another failed way to do, may be express version is not supporting

---
+++ models/user.js
@@ -0,0 +1,73 @@
+var mongoose = require('mongoose');
+var database = mongoose.connect('mongodb://localhost:27017/nodeauth',{useNewUrlParser:true});
+var bcrypt = require('bcrypt');
+var db = mongoose.connection;
+
+//User Schema
+var UserSchema = mongoose.Schema({
+  username:{
+    type:String,
+    index: true
+  },
+  password:{
+    type:String,
+    required:true,
+    bcrypt:true
+  },
+  email:{
+    type:String
+  },
+  name:{
+    type:String
+  },
+  profileimage:{
+    type:String
+  }
+});
+
+
+
+
+
+
+UserSchema.statics.findByCredentials = function(username, password)
+{
+  var user = this;
+  console.log('Enter the statics')
+  return  Users.findOne({username}).then((user) =>{
+    if(!user)
+    {
+      return Promise.reject((new Error('fail')));
+      console.log('user not found');
+    }
+
+    return new Promise((resolve, reject) =>{
+      bcrypt.compare(password, user.password,(err, res)=>{
+        if(res)
+        {
+          resolve(user);
+          console.log('User found and password matched')
+        }
+        else{
+          reject((new Error('fail')));
+          console.log('Password not matched')
+        }
+      })
+    })
+  })
+};
+
+
+
+module.exports.createUser = (newUser,callback)=>{
+  bcrypt.hash(newUser.password,10,(err, hash)=>{
+    if(err) throw err;
+    //set hashed pw
+    newUser.password = hash;
+    newUser.save(callback);
+  })
+
+
+};
+
+var User = module.exports = mongoose.model('User', UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 var express = require('express');
+const { body } = require('express-validator/check');
 var router = express.Router();
+var _ = require('lodash');
+var User = require('../models/user');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -19,6 +23,89 @@ router.get('/login', (req, res,next)=>{
     'title':'login'
   });
 });
+//
+// router.post('/login',(req,res, next) =>{
+//   res.send('<h1>working here</h1>');
+// })
 
+router.post('/login', async(req, res)=>{
+      const body = _.pick(req.body,['username','password']);
+  try{
+    const user = await User.findByCredentials(body.email, body.password);
+    console.log(user);
+    res.location('/');
+    res.redirect('/');
+  }
+  catch(e)
+  {
+    res.status(400).send(e);
+  }
+})
+
+router.post('/register',(req, res, next)=>{
+  //Get values form values
+
+  var name  = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  //Check for Image Field
+  if(req.files.profileimage)
+  {
+    console.log('Uploading File...');
+
+    //File Info
+    var profileImageOriginalName      =req.files.profileimage.profileImageOriginalName;
+    var profileImageName              =req.files.profileimage.name;
+    var profileImageMime              =req.files.profileimage.mimeType;
+    var profileImagePath              =req.files.profileimage.path;
+    var profileImageExt               =req.files.profileimage.extension;
+    var profileImageSize              =req.files.profileimage.size;
+  }
+  else{
+    //Set a Default Image
+    var profileImageName = 'noImage.png';
+  }
+    //Form Validation
+    req.checkBody('name','Name Field is required').notEmpty();
+    req.checkBody('email','Email Field is required').notEmpty();
+    req.checkBody('email','Email not valid').isEmail();
+    req.checkBody('username','username Field is required').notEmpty();
+    req.checkBody('password','Password Field is required').notEmpty();
+    req.checkBody('password2','Password do not match').equals(req.body.password);
+    //Check for errors
+    var errors = req.validationErrors();
+    if(errors)
+    {
+      res.render('register',{
+        errors:errors,
+        name,
+        email,
+        username,
+        password,
+        password2
+      });
+    }else{
+      var newUser = new User({
+        name,
+        email,
+        username,
+        password,
+        profileimage: profileImageName
+      });
+
+      // Create username
+      User.createUser(newUser, (err, user)=>{
+        if(err) throw err;
+        console.log(user);
+      });
+      req.flash('Suceesss', 'You are now registered and you may log in');
+
+      res.location('/');
+      res.redirect('/');
+    }
+});
 
 module.exports = router;
